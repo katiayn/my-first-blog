76c22df635984bf694d41c980d5027b7227831c1
Kátia Nakamura <katiayn@gmail.com>
2015-11-07 14:58:07 -0200

    Added views to create/edit Blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -5,14 +5,12 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	created_date = models.DateTimeField(
-		default=timezone.now)
-	published_date = models.DateTimeField(
-		blank=True, null=True)
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
 
-def publish(self):
-	self.published_date = timezone.now()
-	self.save()
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
 
-def __str__(self):
-	return self.title
+	def __str__(self):
+		return self.title
